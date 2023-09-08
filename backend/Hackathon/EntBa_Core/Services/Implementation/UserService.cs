a50d908fa90b8a2b1aa7d325f2c93a7d9a92ad59
Lukáš Cauner <ext.lcauner@partners.cz>
2023-09-08 23:44:25 +0200

    Recaptcha v3 added

---
+++ backend/Hackathon/EntBa_Core/Services/Implementation/UserService.cs
@@ -0,0 +1,33 @@
+﻿using EntBa_Core.Database;
+using EntBa_Core.ModelsLogic.User;
+using EntBa_Core.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace EntBa_Core.Services.Implementation
+{
+    public class UserService : BaseService, IUserService
+    {
+        private readonly string _recaptchaSecretKey;
+
+        private HttpClient? _recaptchaClient;
+        private HttpClient RecaptchaClient => _recaptchaClient ??= new HttpClient
+        {
+            BaseAddress = new Uri("https://www.google.com/recaptcha/api/siteverify")
+        };
+
+        public UserService(EntBaDbContext dbContext, IConfiguration configuration) : base(dbContext)
+        {
+            _recaptchaSecretKey = configuration["RecaptchaSecretKey"]!;
+        }
+        public async Task<bool> RecaptchaVerification(string token, string action)
+        {
+            var response = await RecaptchaClient.PostAsync($"?secret={_recaptchaSecretKey}&response={token}", null);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var recaptchaResponse = Newtonsoft.Json.JsonConvert.DeserializeObject<RecaptchaResponse>(responseBody);
+            var success = recaptchaResponse?.Success ?? false;
+            var actionEquals = recaptchaResponse?.Action == action;
+            return success && actionEquals;
+        }
+    }
+}
---
+++ backend/Hackathon/EntBa_WebBackend/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using EntBa_Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntBa_WebBackend.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("/recaptcha/{token}/{action}")]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<bool>> RecaptchaVerification(string token, string action)
+        {
+            ActionResult result;
+            try
+            {
+                result = Ok(await _userService.RecaptchaVerification(token, action));
+            }
+            catch (HttpRequestException)
+            {
+                result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+            return result;
+        }
+    }
+}
