b3801f9b70e542d205d7b6886f3ab1fe56cb8d5e
Oreský Adam <adam.oresky@bait.sk>
2023-09-09 00:49:32 +0200

    Bugfix + prepared other service endpoints

--- frontend/src/Service/BackendService.ts
+++ frontend/src/Service/BackendService.ts
@@ -5,8 +5,21 @@ class SongbookBackendService{
     static config = require("../config.json");
     static serverUrl = SongbookBackendService.config.serverUrl;
 
-    static CreateRegistrationLink(): Promise<AxiosResponse<RegistrationResultEnum>>{
-        return axios.get<RegistrationResultEnum>(this.serverUrl+"Author/allForAnalysis");
+    // region Registration
+
+    static VerifyLink(link: string): Promise<AxiosResponse<RegistrationResultEnum>>{
+        return axios.get<RegistrationResultEnum>(this.serverUrl+"");
     }
+
+    static CreateRegistrationLink(email: string): Promise<AxiosResponse<RegistrationResultEnum>>{
+        return axios.post<RegistrationResultEnum>(this.serverUrl+"");
+    }
+
+    static Register(user: string): Promise<AxiosResponse<RegistrationResultEnum>>{
+        return axios.post<RegistrationResultEnum>(this.serverUrl+"");
+    }
+
+    // endregion
+
 }
 export default  SongbookBackendService;
