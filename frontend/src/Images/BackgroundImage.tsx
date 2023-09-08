fc78a09d0efa614515fa64f463ea13322aebe8b6
Oreský Adam <adam.oresky@bait.sk>
2023-09-08 21:13:19 +0200

    Added BackgroundImage.tsx for easier multiple usage

---
+++ frontend/src/Images/BackgroundImage.tsx
@@ -0,0 +1,10 @@
+const BackgroundImage = () => {
+    return (
+        <img className={"bgImage"}
+             src={require('../Images/BA.png')}
+             alt={"bg image"}
+        />
+    )
+}
+
+export default BackgroundImage;
