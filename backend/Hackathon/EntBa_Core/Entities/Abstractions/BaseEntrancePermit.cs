ce5cc0d21cd14bb7a3d6504f37d76cb33b4df34b
Trenčanský Peter <peter.trencansky@bait.sk>
2023-09-08 15:13:28 +0200

    Models update

--- backend/Hackathon/EntBa_Core/Entities/Abstractions/BaseEntrancePermit.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EntBa_Core.Entities.Abstractions;
-
-public abstract class BaseEntrancePermit: BaseDbo
-{
-    public DateTime ValidFrom { get; set; }
-    public DateTime ValidTo { get; set; }
-    public required string LicensePlate { get; set; }
-}
---
+++ backend/Hackathon/EntBa_Core/Entities/EntrancePermits/ResidentEntrancePermitDbo.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using EntBa_Core.Entities.EntrancePermits.Abstractions;
+using EntBa_Core.Entities.SystemUsers;
+
+namespace EntBa_Core.Entities.EntrancePermits;
+
+public class ResidentEntrancePermitDbo: BaseEntrancePermitDbo
+{
+    public int UserId { get; set; }
+    [ForeignKey(nameof(UserId))]
+    public UserDbo? User { get; set; }
+    public required string OwnerDeclarationNumber { get; set; }
+}
