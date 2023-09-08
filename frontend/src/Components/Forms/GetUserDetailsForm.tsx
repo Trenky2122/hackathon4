f1724b765844845f2d65a2f00a7f084bab79def1
Oreský Adam <adam.oresky@bait.sk>
2023-09-08 16:10:24 +0200

    Shows Successful Pop Up after registration

--- frontend/src/Components/Forms/GetUserDetailsForm.tsx
+++ frontend/src/Components/Forms/GetUserDetailsForm.tsx
@@ -3,6 +3,7 @@ import {Button, Container, Nav, Navbar, Offcanvas} from "react-bootstrap";
 import {Link, useNavigate, useParams} from "react-router-dom";
 import LocalizedStrings from "react-localization";
 import TextField from "@material-ui/core/TextField";
+import SuccessfulPopUp from "../PopUp/SuccessfulPopUp";
 
 const GetUserDetailsForm = () => {
     let navigate = useNavigate();
@@ -10,6 +11,7 @@ const GetUserDetailsForm = () => {
     let [surname, setSurname] : [string, any] = useState("");
     let [email, setEmail] : [string, any] = useState("");
     let [password, setPassword] : [string, any] = useState("");
+    let [registrationSuccessful, setRegistrationSuccessful] : [boolean, any] = useState(false);
     const { verificationKey } = useParams();
 
     const localization = new LocalizedStrings({
@@ -23,26 +25,42 @@ const GetUserDetailsForm = () => {
 
     useEffect(() => {
         console.log(verificationKey)
+        // Zavolaj backend service aby zistil ze ci existuje taky verif key
+        // AK existuje tak daj aj email nech ho mozem vlozit do premennej email
+        setEmail("TODO: Email z DB")
     }, [])
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault()
+        setRegistrationSuccessful(true)
+    }
 
     return (
         <div>
-            <div>{localization.title}</div>
-            <TextField style={{width: 500}} label={"Meno"} required autoFocus value={name}
-                onChange={(e) => {
-                    setName(e.target.value)
-                }}/>
-            <TextField style={{width: 500}} label={"Priezvisko"} required value={surname}
-                       onChange={(e) => {
-                           setSurname(e.target.value)
-                       }}/>
-            <TextField style={{width: 500}} label={"Email"} required disabled value={"email z databázy"}/>
-            <TextField style={{width: 500}} label={"Heslo"} value={password}
-                       onChange={(e) => {
-                           setPassword(e.target.value)
-                       }}/>
-            <Button className={"me-2"} variant={"success"} onClick={() => console.log("")}>Uložiť</Button>
-
+            <form onSubmit={handleSubmit}>
+                <div>{localization.title}</div>
+                <TextField style={{width: 500}} label={"Meno"} required autoFocus value={name}
+                           onChange={(e) => {
+                               setName(e.target.value)
+                           }}/>
+                <TextField style={{width: 500}} label={"Priezvisko"} required value={surname}
+                           onChange={(e) => {
+                               setSurname(e.target.value)
+                           }}/>
+                <TextField style={{width: 500}} label={"Email"} required disabled value={email}/>
+                <TextField style={{width: 500}} label={"Heslo"} value={password}
+                           onChange={(e) => {
+                               setPassword(e.target.value)
+                           }}/>
+                <Button className={"me-2"} type={"submit"} variant={"success"} onClick={() => console.log("")}>Dokončiť registráciu</Button>
+            </form>
+            <SuccessfulPopUp
+                TitleText={"Registrácia úspešná"}
+                BodyText={"Gratulujeme. Úspešne ste zaregistrovaný. Môžete požiadať o žiadosť."}
+                ButtonUrl={"/profil"}
+                ButtonText={"Môj profil"}
+                show={registrationSuccessful}
+            />
         </div>
     )
 }
