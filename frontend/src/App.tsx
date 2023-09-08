fb686419a0379509b1a9e787db8eaa72d4b28b25
Oreský Adam <adam.oresky@bait.sk>
2023-09-08 18:32:44 +0200

    Adds navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,6 +19,7 @@ function App() {
     return (
         <div className={"wrapper"}>
             <BrowserRouter>
+                <NavigationComponent/>
                 <Routes>
                     <Route path="/" element={<Home/>}></Route>
 
