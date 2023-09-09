b6c78baf7d8de66643be5182dd2060211b7ca9ec
Lukáš Cauner <ext.lcauner@partners.cz>
2023-09-09 14:57:47 +0200

    Entrance request - base ui

---
+++ backend/Hackathon/EntBa_Core/ModelsLogic/Admin/EntranceRequestBase.cs
@@ -0,0 +1,16 @@
+﻿using EntBa_Core.Enums;
+
+namespace EntBa_Core.ModelsLogic.Admin
+{
+    public class EntranceRequestBase
+    {
+        public int Id { get; set; }
+        public required RequestStateEnum RequestState { get; set; }
+        public required RequestTypeEnum RequestType { get; set; }
+        public required string RequesterName { get; set; }
+        public required string RequesterSurname { get; set; }
+        public required string Caption { get; set; }
+        public required DateTime Date { get; set; }
+
+    }
+}
---
+++ backend/Hackathon/EntBa_WebBackend/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using EntBa_Core.Enums;
+using EntBa_Core.ModelsLogic.Admin;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntBa_WebBackend.Controllers
+{
+    [Route("admin")]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("entranceRequestsBase")]
+        public async Task<IList<EntranceRequestBase>> GetEntranceRequestsBase()
+        {
+            return await Task.FromResult(new List<EntranceRequestBase>
+            {
+                new()
+                {
+                    Id = 1,
+                    Caption = "Testovací vstup",
+                    Date = DateTime.Now,
+                    RequesterName = "Ján",
+                    RequesterSurname = "Vysoký",
+                    RequestState = RequestStateEnum.WaitingForApproval,
+                    RequestType = RequestTypeEnum.Resident
+                }
+            });
+        }
+
+    }
+}
