26fdb44c5a6eea1d89d0d0e9415639d12d29a2d5
Oreský Adam <adam.oresky@bait.sk>
2023-09-09 04:35:42 +0200

    GetUserDetailsForm UI + ID number

--- frontend/src/Components/Forms/GetUserDetailsForm.tsx
+++ frontend/src/Components/Forms/GetUserDetailsForm.tsx
@@ -11,6 +11,7 @@ const GetUserDetailsForm = () => {
     let [surname, setSurname] : [string, any] = useState("");
     let [email, setEmail] : [string, any] = useState("");
     let [password, setPassword] : [string, any] = useState("");
+    let [idNumber, setIdNumber] : [string, any] = useState("");
     let [registrationSuccessful, setRegistrationSuccessful] : [boolean, any] = useState(false);
     const { verificationKey } = useParams();
 
@@ -33,35 +34,57 @@ const GetUserDetailsForm = () => {
 
     const handleSubmit = (e: any) => {
         e.preventDefault()
+        //send data to backend to create user
         setRegistrationSuccessful(true)
+        //set localstorage as logged-in user
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>{localization.title}</div>
-                <TextField style={{width: 500}} label={"Meno"} required autoFocus value={name}
-                           onChange={(e) => {
-                               setName(e.target.value)
-                           }}/>
-                <TextField style={{width: 500}} label={"Priezvisko"} required value={surname}
-                           onChange={(e) => {
-                               setSurname(e.target.value)
-                           }}/>
-                <TextField style={{width: 500}} label={"Email"} required disabled value={email}/>
-                <TextField style={{width: 500}} label={"Heslo"} value={password}
-                           onChange={(e) => {
-                               setPassword(e.target.value)
-                           }}/>
-                <Button className={"me-2"} type={"submit"} variant={"success"} onClick={() => console.log("")}>Dokončiť registráciu</Button>
-            </form>
-            <MessagePopUp
-                TitleText={"Registrácia úspešná"}
-                BodyText={"Gratulujeme. Úspešne ste zaregistrovaný. Môžete požiadať o žiadosť."}
-                ButtonUrl={"/profil"}
-                ButtonText={"Môj profil"}
-                show={registrationSuccessful}
-            />
+        <div className={"container"}>
+            <div className={"row justify-content-center"} >
+                <div className={"col-6 homepageForm"}>
+                    <form onSubmit={handleSubmit}>
+                        <h1 style={{marginBottom: "30px"}}>{localization.title} </h1>
+                        <div className={"row mb-3"}>
+                            <div className={"col-6"}>
+                                <TextField label={"Meno"} required autoFocus value={name}
+                                           onChange={(e) => {
+                                               setName(e.target.value)
+                                           }}/>
+                            </div>
+                            <div className={"col-6"}>
+                                <TextField label={"Priezvisko"} required value={surname}
+                                           onChange={(e) => {
+                                               setSurname(e.target.value)
+                                           }}/>
+                            </div>
+                        </div>
+
+                        <div className={"row mb-3"}>
+                            <div className={"col-6"}>
+                                <TextField label={"Email"} disabled value={email}/>
+                            </div>
+                            <div className={"col-6"}>
+                                <TextField label={"Heslo"} value={password} required
+                                           onChange={(e) => {
+                                               setPassword(e.target.value)
+                                           }} type={"password"} />
+                            </div>
+                        </div>
+
+                        <div className={"row mb-3 justify-content-center"}>
+                            <div className={"col-8"}>
+                                <TextField label={"Číslo občianskeho"} required value={idNumber}
+                                           onChange={(e) => {
+                                               setIdNumber(e.target.value)
+                                           }}/>
+                            </div>
+                        </div>
+                        <Button className={"me-2"} type={"submit"} variant={"success"} onClick={() => console.log("")}>Dokončiť registráciu</Button>
+                    </form>
+                </div>
+            </div>
+            <MessagePopUp TitleText={"Registrácia úspešná"} BodyText={"Gratulujeme. Úspešne ste sa registrovali."} ButtonUrl={"/profil"} show={registrationSuccessful}/>
         </div>
     )
 }
