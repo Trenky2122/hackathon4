04d32e4455214460168c747e666f37a886b674c7
Oreský Adam <adam.oresky@bait.sk>
2023-09-09 03:26:49 +0200

    User email clearing code

--- frontend/src/Components/Forms/GetUserEmailForm.tsx
+++ frontend/src/Components/Forms/GetUserEmailForm.tsx
@@ -4,14 +4,11 @@ import {useNavigate} from "react-router-dom";
 import LocalizedStrings from "react-localization";
 import TextField from "@material-ui/core/TextField";
 import MessagePopUp from "../PopUp/MessagePopUp";
-import BackgroundImage from "../../Images/BackgroundImage";
 import {RegistrationResultEnum} from "../../Models/Models";
 
 const GetUserEmailForm = () => {
     let navigate = useNavigate();
     let [email, setEmail] : [string, any] = useState("");
-    let [emailSent, setEmailSent] : [boolean, any] = useState(false);
-    let [errorMessage, setErrorMessage] : [string, any] = useState(" ");
     let [serverResponse, setServerResponse] : [any, any?] = useState(null);
     const localization = new LocalizedStrings({
         en: {
@@ -24,16 +21,10 @@ const GetUserEmailForm = () => {
 
     const handleSubmit = (e: any) => {
         e.preventDefault()
-        if(true){
-            console.log("Posielam mail na", email)
-            // Zavolaj backend endpoint s parametrom mail
-            // Backend endpiont nech vytvori gui, a prida data ako mail, cas, gui do tabulky
-            setEmailSent(true)
-            setServerResponse(RegistrationResultEnum.Ok)
-        }
-        else{
-            setErrorMessage("Neplatný mail")
-        }
+        console.log("Posielam mail na", email)
+        // Zavolaj backend endpoint s parametrom mail
+        // Backend endpiont nech vytvori gui, a prida data ako mail, cas, gui do tabulky
+        setServerResponse(RegistrationResultEnum.Ok)
     }
 
     return (
@@ -43,11 +34,10 @@ const GetUserEmailForm = () => {
                     <div className={"col-3 homepageForm"}>
                         <form onSubmit={handleSubmit}>
                         <h1 style={{marginBottom: "30px"}}>{localization.title} </h1>
-                            <TextField className={"mb-3"} label={"Email"} required value={email}
+                            <TextField className={"mb-4"} label={"Email"} required value={email}
                                        onChange={(e) => {
                                            setEmail(e.target.value);
-                                           setErrorMessage(" ");
-                                       }} error={errorMessage != " "} helperText={errorMessage} fullWidth={true} />
+                                       }} type={"email"} fullWidth={true} />
                             <Button className={"me-2"} variant={"danger"} onClick={() => navigate("/")}>Zrušiť</Button>
                             <Button className={"me-2"} type={"submit"} variant={"success"}>Over mail</Button></form>
                     </div>
