52e7fe6364e3c92ebd7714421df456431b05b0c6
Oreský Adam <adam.oresky@bait.sk>
2023-09-09 07:14:45 +0200

    Permit nit granted UI

--- frontend/src/Components/ErrorPages/PermitNotGranted.tsx
+++ frontend/src/Components/ErrorPages/PermitNotGranted.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import {Button} from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const PermitNotGrantedComponent = () => {
     let navigate = useNavigate();
     return (
-        <div>
-            <div>Prepáčte, na povolenie nemáte nárok</div>
-            <Button className={"me-2"} variant={"success"} onClick={() => navigate("/")}>Homepage</Button>
+        <div className={"container"}>
+            <div className={"row justify-content-center"} >
+                <div className={"col-6 homepageForm"}>
+                    <h1 className={"mb-4"}>Prepáčte, na povolenie nemáte nárok</h1>
+                    <Button className={"me-2"} variant={"success"} onClick={() => navigate("/")}>Domov</Button>    </div>
+            </div>
         </div>
     )
 }
